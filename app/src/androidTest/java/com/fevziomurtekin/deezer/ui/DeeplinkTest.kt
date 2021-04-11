81c917d14a896c30d7a3bf4cbff97a45cfb6ed61
fevziomurtekin <fevziomurtekin@gmail.com>
2021-04-11 17:36:39 +0300

    Add deeplink test scenario

--- app/src/androidTest/java/com/fevziomurtekin/deezer/ui/DeeplinkTest.kt
+++ app/src/androidTest/java/com/fevziomurtekin/deezer/ui/DeeplinkTest.kt
@@ -5,9 +5,12 @@ import android.net.Uri
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.ViewAssertion
 import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -42,7 +45,7 @@ class DeeplinkTest {
 
 
     @Test
-    fun openApp_DeepLink_HandlesIntent_BackGoesHomeScreen() {
+    fun openApp_withGenereDetails_DeepLink_HandlesIntent_BackGoesHomeScreen() {
         //www.example.com/genre/{id}/{name}
 
         //given
@@ -56,16 +59,56 @@ class DeeplinkTest {
         activityRule.launchActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
 
         //then
-        Assert.assertEquals(
-            activityRule.activity?.materialToolbar?.title,
-            name
-        )
-
+        onView(withId(R.id.materialToolbar))
+            .check(matches(hasDescendant(withText(name))))
         Espresso.pressBack()
-        Assert.assertEquals(
-            activityRule.activity?.materialToolbar?.title,
-            defaultTitle
-        )
+        onView(withId(R.id.materialToolbar))
+            .check(matches(hasDescendant(withText(defaultTitle))))
+
+    }
+
+    @Test
+    fun openApp_withArtistDetails_DeepLink_HandlesIntent_BackGoesGenreScreen() {
+        //www.example.com/genre/{id}/{name}
+
+        //given
+        val name = "Ezhel"
+        val id = "8354140"
+        val url = "https://www.deezerclone.com/artist/${id}/${name}"
+        activityRule = ActivityTestRule(MainActivity::class.java)
+
+        //when
+        activityRule.launchActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
+
+        //then
+        onView(withId(R.id.materialToolbar))
+            .check(matches(hasDescendant(withText(name))))
+        Espresso.pressBack()
+        onView(withId(R.id.materialToolbar))
+            .check(matches(hasDescendant(withText(name))))
+
+    }
+
+    fun openApp_withAlbumDetails_DeepLink_HandlesIntent_BackGoesGenreScreen() {
+        //www.example.com/genre/{id}/{name}
+
+        //given
+        val defaultTitle = "Deezer Clone"
+        val name = "Ezhel"
+        val id = "8354140"
+        val url = "https://www.deezerclone.com/artist/${id}/${name}"
+        activityRule = ActivityTestRule(MainActivity::class.java)
+
+        //when
+        activityRule.launchActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
+
+        //then
+        onView(withId(R.id.materialToolbar))
+            .check(matches(hasDescendant(withText(name))))
+        Espresso.pressBack()
+        onView(withId(R.id.materialToolbar))
+            .check(matches(hasDescendant(withText(name))))
+
     }
 
 }
