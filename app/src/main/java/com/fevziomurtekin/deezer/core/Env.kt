9d24d6d7be25a944fec62776757202e63c2315c3
fevziomurtekin <fevziomurtekin@gmail.com>
2021-04-24 19:25:07 +0300

    Update .gitignore

---
+++ app/src/main/java/com/fevziomurtekin/deezer/core/Env.kt
@@ -0,0 +1,26 @@
+package com.fevziomurtekin.deezer.core
+
+/**
+ * @author: fevziomurtekin
+ * @date: 24/04/2021
+ */
+
+object Env {
+
+    const val APP_NAME          = "Deezer Clone"
+
+    /* database*/
+    const val DATABASE_NAME     = "deezerdatabase.db"
+    const val DATABASE_VERSION  = 6
+
+    /* API ENV */
+    const val BASE_URL          = "https://api.deezer.com/"
+
+    /* keywords of bundle */
+    const val BUND_NAME         = "name"
+    const val BUND_ID           = "id"
+
+    /* Default value */
+    const val DEFAULT_IMG       = "https://www.ssbwiki.com/images/2/29/Music.svg"
+
+}
